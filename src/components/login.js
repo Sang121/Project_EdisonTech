59e2523add8be46fd54e85b1d3327ef7f6169a21
huyhoangm2 <hoangken760@gmail.com>
2023-03-19 18:30:39 +0700

    sửa css login

--- src/components/login.js
+++ src/components/login.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import styles from "./login.module.css";
+
+
 const LoginForm = () => {
-  const initialValues = { email: "", password: "" };
+  const initialValues = { email: "", password: "", rememberMe: false };
 
   const validationSchema = Yup.object({
     email: Yup.string()
@@ -15,33 +17,57 @@ const LoginForm = () => {
 
   const onSubmit = (values, { setSubmitting }) => {
     setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
+      alert( `Email: ${values.email}\nPassword: ${values.password}\nRemember me: ${
+        values.rememberMe ? "Yes" : "No"
+      }`);
       setSubmitting(false);
     }, 400);
   };
 
   return (
-    <div className={styles.khung}>
-    
+    <div className={styles.container}>
+    <div className={styles.logo}  >
+      hbjhjh
+    </div>
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
       {({ isSubmitting }) => (
-        <Form className={styles.container}>
+
+        <Form className={styles.form}>
           <h1 className={styles.h1}>Login</h1>
-          <div>
-            <Field type="email" placeholder='Email' name="email" className={styles.input} />
+          
+         
+          <div className={styles.item}>
+          
+            <Field type="email"  name="email" className={styles.input} />
+            <p className={styles.trong}></p>
             <ErrorMessage name="email" component="div" className={styles.error} />
+            <label>Email</label>
+          </div>
+          <div className={styles.item} >
+            <Field type="password"  name="password" className={styles.input} />
+            <p className={styles.trong}></p>
+            <ErrorMessage name="password" component="div" className={styles.error} />
+            <label>Password</label>
           </div>
           <div>
-            <Field type="password" placeholder='Password' name="password" className={styles.input} />
-            <ErrorMessage name="password" component="div" className={styles.error} />
+
+            <Field type="checkbox" name="rememberMe" className={styles.checkbox} />
+        <label htmlFor="rememberMe" >
+          Remember me
+        </label>
           </div>
-          <button type="submit" disabled={isSubmitting} className={styles.btnSubmit}>
-            Login
-          </button>
+
+            <button type="submit" disabled={isSubmitting}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        login
+      </button>
         </Form>
       )}
     </Formik>
