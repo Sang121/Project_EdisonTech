7ee206dad8ecc6a3b57e296430cd6c021ff2d7ea
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-03-20 00:43:49 +0700

    add logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
 import {
-  createBrowserRouter,RouterProvider
-} from "react-router-dom";   
+  createBrowserRouter, RouterProvider
+} from "react-router-dom";
 import Home from './pages/Home/Home';
 import './App.css';
 import ProductDetails from './pages/productDetail/productDetail';
@@ -12,35 +12,30 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>
+      element: <Home />
     },
     {
       path: '/login',
-      element:<Login/>
+      element: <Login />
     },
     {
       path: '/product/:id',
-      element:<ProductDetails/>
+      element: <ProductDetails />
     },
     {
       path: '/register',
-      element:<Register/>
+      element: <Register />
     },
     {
       path: '/editProfile',
-      element:<ProfileForm/>
+      element: <ProfileForm />
     }
 
   ]);
   return (
     <div>
-   
-    <RouterProvider
-
-      router={router}
-    >
-    </RouterProvider>
+      <RouterProvider router={router} />
     </div>
-    );
+  );
 }
 export default App;
--- src/pages/productDetail/productDetail.jsx
+++ src/pages/productDetail/productDetail.jsx
@@ -1,80 +1,136 @@
 import Header from '../../components/Headers'
 import Footer from '../../components/footer';
-   function ProductDetail(){
-    
-   
-      return (
-        <div>
-            <Header/>
-        <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <ul className="nav nav-tabs nav-justified">
-                  <li className="nav-item">
-                    <a className="nav-link" href="cat1.html">Product Category 1</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="cat1.html">Product Category 2</a>
-                  </li><li className="nav-item">
-                    <a className="nav-link" href="cat2.html">Product Category 3</a>
-                  </li>
-                  <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                  </form>
-                </ul>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-3">
-                <div className="card">
-                  <img src="C://Users/admin/Desktop/lab html/product_details/tui1.jpg" alt="Lacos" title="Túi Lacos" />
-                </div>
-              </div>
-              <div className="col-9">
-                <h1>Tên sản phẩm: Quần áo nam</h1>
-                <p>Giá sản phẩm: 1.000$</p>
-                <div className="btn-group" role="group" aria-label="Basic outlined example">
-                  <h2>Màu sắc &nbsp;</h2> 
-                  <button type="button" className="btn btn-outline-primary">Black</button>
-                  <button type="button" className="btn btn-outline-primary">Green</button>
-                  <button type="button" className="btn btn-outline-primary">White</button>
-                </div><br />
-                <div className="btn-group" role="group" aria-label="Basic outlined example">
-                  <h2>Kích cỡ &nbsp;</h2> 
-                  <button type="button" className="btn btn-outline-primary">M</button>
-                  <button type="button" className="btn btn-outline-primary">L</button>
-                  <button type="button" className="btn btn-outline-primary">XL</button>
-                </div>
-                <div>
-                  <button type="button">Add to cart</button>
-                </div>
-                <div>
-                  <p>
-                    <button onclick="totalClick(-1)">-</button>
-                    <span id="totalClicks">1</span>
-                    <button onclick="totalClick(1)">+</button>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <ul className="nav nav-tabs">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">DESCRIPTION</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">POLICY</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">CONTACT</a>
-                </li>
-              </ul>
-            </div>
-            
-          </div>
-          <Footer/>
-          </div>
-      );
+import ProductList from '../../components/ProductList';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import './productDetail.css';
+const base_url = 'https://dummyjson.com';
+
+function ProductDetail() {
+  const [product, setProduct] = useState([])
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      try {
+        axios.get(`${base_url}/products/${id}`)
+          .then(response => {
+            setProduct(response.data);
+
+          })
+      }
+      catch (error) {
+        console.log(error);
+      }
     }
+
+    return () => {
+      console.log("unmount");
+    };
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <ul className="nav nav-tabs nav-justified">
+              <li className="nav-item">
+                <img
+                  src={product.images && product.images.length !== 0 ? product.thumbnail : ""}
+                  alt=""
+                  className="img-cover"
+                />
+              </li>
+              <li className="nav-item">
+
+                <img
+                  className='item'
+                  src={product.images && product.images.length !== 0 ? product.images[0] : ""}
+                  alt=""
+
+                />
+
+              </li>
+              <li className="nav-item">
+
+                <img
+                  className='item'
+                  src={product.images && product.images.length !== 0 ? product.images[1] : ""}
+                  alt=""
+
+                />
+
+              </li>
+              <li className="nav-item">
+
+                <img
+                  className='item'
+                  src={product.images && product.images.length !== 0 ? product.images[2] : ""}
+                  alt=""
+
+                />
+
+              </li>
+              <li className="nav-item">
+
+                <img
+                  className='item'
+                  src={product.images && product.images.length !== 0 ? product.images[3] : ""}
+                  alt=""
+
+                />
+
+              </li>
+
+
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-3">
+            <div className="card">
+              <img src={product.thumbnail} alt="Lacos" title="Túi Lacos" />
+            </div>
+          </div>
+          <div className="col-9">
+            <h1>{product.title}</h1>
+            <p>Giá sản phẩm: {(product.price * (100 - product.discountPercentage) / 100).toFixed(2)}$</p>
+            <div className="btn-group" role="group" aria-label="Basic outlined example">
+              <h2>Màu sắc &nbsp;</h2>
+              <button type="button" className="btn btn-outline-primary">Black</button>
+              <button type="button" className="btn btn-outline-primary">Green</button>
+              <button type="button" className="btn btn-outline-primary">White</button>
+            </div><br />
+            <div className="btn-group" role="group" aria-label="Basic outlined example">
+              <h2>Kích cỡ &nbsp;</h2>
+              <button type="button" className="btn btn-outline-primary">M</button>
+              <button type="button" className="btn btn-outline-primary">L</button>
+              <button type="button" className="btn btn-outline-primary">XL</button>
+            </div>
+            <div>
+              <button type="button">Add to cart</button>
+            </div>
+            <div>
+              <p>
+                <button onclick="totalClick(-1)">-</button>
+                <span id="totalClicks">1</span>
+                <button onclick="totalClick(1)">+</button>
+              </p>
+            </div>
+            <h4> {product.description}</h4>
+          </div>
+
+        </div>
+
+
+      </div>
+      <Footer />
+    </div>
+  )
+}
 export default ProductDetail
