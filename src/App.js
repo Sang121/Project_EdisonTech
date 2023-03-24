5fd90a1a09dcecb1b3aec349ec49002d0d809581
huyhoangm2 <hoangken760@gmail.com>
2023-03-24 15:15:46 +0700

    test API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router,
+   Switch, Routes, Route, Link } 
+   from "react-router-dom";
 import {
   createBrowserRouter, RouterProvider
 } from "react-router-dom";
@@ -9,7 +11,28 @@ import Login from './pages/Login/login';
 import Register from './pages/register/register';
 import ProfileForm from './pages/profile/profile';
 import Search from './pages/Search';
+import {userState} from 'react';
+
+
+
+
+function setToken(userToken){
+sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken(){
+const tokenString = sessionStorage.getItem('token');
+const userToken = JSON.parse(tokenString);
+return userToken?.token
+}
+
+
 function App() {
+  // const [token, getToken] = userState();
+
+  // if (!token) {
+  //   return <Login setToken={setToken} />;
+  // }
   const router = createBrowserRouter([
     {
       path: "/",
--- src/components/login.js
+++ src/components/login.js
@@ -3,6 +3,22 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import styles from "./login.module.css";
 import logo from '../assets/logo.png'
+import { PropTypes } from "prop-types";
+import {useState} from 'react';
+
+
+
+async function loginUser(credentials) {
+  return fetch('http://localhost:8080/login', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+  .then(data => data.json())
+}
+
 
 const LoginForm = () => {
   const initialValues = { email: "", password: "", rememberMe: false };
@@ -24,6 +40,18 @@ const LoginForm = () => {
     }, 400);
   };
 
+const [username, setUserName] = useState();
+const [password, setPassword] = useState();
+// const[token,setToken] = useState();
+const handleSubmit = async e => {
+  e.preventDefault();
+  const token = await loginUser({
+    username,
+    password
+  });
+  setToken(token);
+}
+
   return (
     <div className={styles.container}>
     <div className={styles.logo}  >
@@ -36,19 +64,19 @@ const LoginForm = () => {
     >
       {({ isSubmitting }) => (
 
-        <Form className={styles.form}>
+        <Form className={styles.form} onSubmit={handleSubmit} >
           <h1 className={styles.h1}>Login</h1>
           
          
           <div className={styles.item}>
           
-            <Field type="email"  name="email" className={styles.input} />
+            <Field type="email" onChange={e => setUserName(e.target.value)} name="email" className={styles.input} />
             <p className={styles.trong}><ErrorMessage name="email" component="div" className={styles.error} /></p>
             
             <label>Email</label>
           </div>
           <div className={styles.item} >
-            <Field type="password"  name="password" className={styles.input} />
+            <Field type="password" onChange={e => setPassword(e.target.value)} name="password" className={styles.input} />
             <p className={styles.trong}><ErrorMessage name="password" component="div" className={styles.error} /></p>
             
             <label>Password</label>
@@ -74,5 +102,8 @@ const LoginForm = () => {
     </div>
   );
 };
-
-export default LoginForm;
+LoginForm.protoTypes = {
+  setToken: PropTypes.func.isRequired
+};
+export default LoginForm();
+  
