af9dbf63f09b961eedfdbfe36c0f828bf00b81d6
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-03-28 10:56:32 +0700

    add header footer

--- src/App.js
+++ src/App.js
@@ -1,45 +1,67 @@
 import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
-import {
-  createBrowserRouter, RouterProvider
-} from "react-router-dom";
+
 import Home from './pages/Home/Home';
 import './App.css';
-import ProductDetails from './pages/productDetail/productDetail';
+
 import Login from './pages/Login/login';
 import Register from './pages/register/register';
 import ProfileForm from './pages/profile/profile';
-import Search from './pages/Search';
-function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/product/:id',
-      element: <ProductDetails/>
-    },
-    {
-      path: '/search/:searchTerm',
-      element: <Search/>
-    },
-    {
-      path: '/register',
-      element: <Register />
-    },
-    {
-      path: '/editProfile',
-      element: <ProfileForm />
-    }
+import ProductSearch from './pages/productSearch/productSearch';
+import Cart from './pages/cart/cart';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import CartPage from './pages/cartPage/cartPage';
+import { useState } from "react";
+import ProductDetail from "./pages/productDetail/productDetail";
 
-  ]);
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    const itemIndex = cartItems.findIndex((i) => i.id === item.id);
+
+    if (itemIndex !== -1) {
+      // If the item already exists in the cart, increase its quantity by 1
+      const updatedCart = [...cartItems];
+      updatedCart[itemIndex].quantity += item.quantity;
+      setCartItems(updatedCart);
+    } else {
+      // If the cart does not exist, create a new array and add the new item to it
+      const updatedCart = [...cartItems, item];
+      setCartItems(updatedCart);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    const updatedCart = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCart);
+  };
+
+  localStorage.setItem('cart', JSON.stringify(cartItems));
+
   return (
     <div>
-      <RouterProvider router={router} />
+      <Router>
+      <Header/> 
+        <Routes>
+         
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/product/:id" element={ <ProductDetail addToCart={addToCart} />} />
+          <Route path="/search/:searchTerm" element={<ProductSearch />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/editProfile" element={<ProfileForm />} />
+          <Route path="/cart" render={() => <Cart cartItems={cartItems} removeFromCart={removeFromCart} />} />
+         
+        </Routes>
+        <Footer/>
+      </Router>
+
+
+
+      
     </div>
   );
 }
